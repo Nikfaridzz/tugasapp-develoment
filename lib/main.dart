04d9842d7ce31803e74673645df41d2f8d34335c
nikfaridzz <nikfaridz180(@gmail.com>
2024-11-21 15:49:01 +0700

    first-commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-//import 'package:tugas/pages/firts_splash.dart';
+import 'package:tugas/pages/firts_splash.dart';
+import 'package:tugas/pages/second_splash.dart';
 import 'package:tugas/pages/signin.dart';
 
 void main() => runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Signin(),
+      home: FirstSplash(),
     );
   }
 }
